0dacd66ce39b05e44ed762b542aebd912dcb4cd9
Nguyen Van Luan <nvluan3@gmail.com>
2025-07-18 11:00:57 +0700

    4

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,24 @@
 const CACHE_NAME = 'qr-telegram-cache-v1';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon.png'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png',
+  'https://unpkg.com/html5-qrcode'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
   );
 });
